433104742e1f7a63c6da8cf8ad3e2af65bf70fde
Lee, Jeong Han <jeonghan.lee@gmail.com>
2025-02-07 00:54:02 -0800

    WIP, overwrite several PVs in IOCSTATS

---
+++ ioc_iocStats.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+# Import the basic framework components.
+import os
+from softioc import softioc, builder
+import cothread
+
+PREFIX="MY-DEVICE_PREFIX"
+
+# Set the record prefix
+builder.SetDeviceName(PREFIX)
+
+# Create some records
+ai = builder.aIn('AI', initial_value=5)
+ao = builder.aOut('AO', initial_value=12.45, on_update=lambda v: ai.set(v))
+
+
+# Boilerplate get the IOC started
+softioc.devIocStats(PREFIX)
+builder.LoadDatabase()
+builder.dbLoadDatabase('db_ioc_stats_replace.db', substitutions=f'P={PREFIX}:')
+softioc.iocInit()
+
+# Start processes required to be run after iocInit
+def update():
+    while True:
+        ai.set(ai.get() + 1)
+        cothread.Sleep(1)
+
+
+cothread.Spawn(update)
+
+# Finally leave the IOC running with an interactive shell.
+softioc.interactive_ioc(globals())
+
